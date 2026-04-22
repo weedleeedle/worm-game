50c8ceb320ceca1f9d372cabb2a89ab3b4904e37
Weedleeedle <11594471+ViperZer0@users.noreply.github.com>
2026-04-22 12:52:06 -0700

    Added ChainIterator

---
+++ src/iterators/chain_iterator.gd
@@ -0,0 +1,28 @@
+## Chains multiple sub-iterators, one after another.
+class_name ChainIterator
+extends Iterator
+
+@export var iterators: Array[Iterator]
+
+var current_iterator := 0
+
+func next() -> IteratorReturn:
+	if current_iterator >= iterators.size():
+		return Iterator.HALT_RETURN
+
+	var next_val = iterators[current_iterator].next()
+	while next_val.is_halt():
+		current_iterator += 1
+		if current_iterator >= iterators.size():
+			return Iterator.HALT_RETURN
+
+		next_val = iterators[current_iterator].next()
+
+	return next_val
+
+func reset() -> void:
+	# Reset all sub-iterators
+	for iterator in iterators:
+		iterator.reset()
+
+	current_iterator = 0
