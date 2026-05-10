f9f50602ce5a6aeafec215783f0da0bff0cafd82
Weedleeedle <11594471+ViperZer0@users.noreply.github.com>
2026-05-10 15:15:27 -0700

    Fixed horn accessories being broken

--- src/accessories/mirror_accessory.gd
+++ src/accessories/mirror_accessory.gd
@@ -9,11 +9,15 @@ extends Accessory
 @export var sub_accessory: Accessory
 
 func init_accessory_model() -> AccessoryModel:
+	var mirror_accessory_root = accessory_model_scene.instantiate()
+	var mirror_accessory_normal = accessory_model_scene.instantiate()
+	var mirror_accessory_mirrored = accessory_model_scene.instantiate()
+	mirror_accessory_mirrored.scale.x = -1 if mirror_x else 1
+	mirror_accessory_mirrored.scale.y = -1 if mirror_y else 1
 	var sub_accessory_model = sub_accessory.init_accessory_model()
 	var sub_accessory_model_2 = sub_accessory.init_accessory_model()
-	sub_accessory_model_2.scale.x = -1 if mirror_x else 1
-	sub_accessory_model_2.scale.y = -1 if mirror_y else 1
-	var accessory_model = accessory_model_scene.instantiate()
-	accessory_model.add_child(sub_accessory_model)
-	accessory_model.add_child(sub_accessory_model_2)
-	return accessory_model
+	mirror_accessory_normal.add_child(sub_accessory_model)
+	mirror_accessory_mirrored.add_child(sub_accessory_model_2)
+	mirror_accessory_root.add_child(mirror_accessory_normal)
+	mirror_accessory_root.add_child(mirror_accessory_mirrored)
+	return mirror_accessory_root
